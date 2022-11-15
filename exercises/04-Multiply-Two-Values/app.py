91ff8c627b287937091070099813b97957bfe6b4
gleonag <gleonag@gmail.com>
2022-11-15 19:05:17 +0000

    done until 11

--- exercises/04-Multiply-Two-Values/app.py
+++ exercises/04-Multiply-Two-Values/app.py
@@ -1,1 +1,4 @@
 # your code here
+variables_are_cool = int(2345) * int(7323)
+print(variables_are_cool)
+
--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,9 @@
 import random
 
 # your code here
+generate_random = random.randint(0, 9)
+print(generate_random)
+
+def generate_random():
+    result = random.randint(0,9)
+    return result
